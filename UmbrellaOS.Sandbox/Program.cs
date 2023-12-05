40154b9f99641d7010e3d51f00235cf899385e20
muzappar <muzappar.ce@foxmail.com>
2023-12-05 15:33:15 +0800

    [Refactor]: clear the project

--- UmbrellaOS.Sandbox/Program.cs
+++ UmbrellaOS.Sandbox/Program.cs
@@ -1,17 +1,9 @@
 ﻿namespace UmbrellaOS.Sandbox
 {
-    internal class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            if (!Directory.Exists("UmbrellaOS"))
-                Directory.CreateDirectory("UmbrellaOS");
-
-            var bootLoader_x86_64 = Temp.GenerateBootLoader_x86_64();
-            File.WriteAllBytes("UmbrellaOS/bootloader_x86_64.bin", bootLoader_x86_64);
-
-            var qemuTest_x86_64 = Temp.GenerateQemuTestScript_x86_64();
-            File.WriteAllText("UmbrellaOS/script_qemu_test_bootloader_x86_64.bat", qemuTest_x86_64);
         }
     }
 }
