3ee9106db3979ce49f68f0432de5939e1a44aa7e
Muzappar <muzappar.ce@foxmail.com>
2023-12-07 17:06:52 +0800

    [Milestone]: successfully generated the image file

--- UmbrellaOS.Sandbox/Program.cs
+++ UmbrellaOS.Sandbox/Program.cs
@@ -4,6 +4,9 @@
     {
         private static void Main(string[] args)
         {
+            var imageStream = File.OpenWrite("umbrella_v0.0.1.iso");
+            Umbrella.Build(imageStream);
+            imageStream.Close();
         }
     }
 }
---
+++ UmbrellaOS/Umbrella.cs
@@ -0,0 +1,22 @@
+﻿using DiscUtils.Iso9660;
+
+namespace UmbrellaOS
+{
+    public static class Umbrella
+    {
+        public static void Build(Stream outputStream)
+        {
+            var builder = new CDBuilder();
+            builder.VolumeIdentifier = "Umbrella";
+            builder.AddDirectory(@"EFI");
+            builder.AddDirectory(@"EFI\BOOT");
+            builder.AddFile(@"EFI\BOOT\BOOTX64.EFI", BuildSystem());
+            builder.Build(outputStream);
+        }
+
+        private static byte[] BuildSystem()
+        {
+            return new byte[0];
+        }
+    }
+}
