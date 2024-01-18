40a4a70f90a9de71bdcf149590078fceb95504ef
Muzappar <muzappar.ce@foxmail.com>
2024-01-18 18:27:45 +0800

    [Feature]: intel instructions encoding part 1

---
+++ UmbrellaOS/Instruction/Intel/Prefixes/PrefixLOCK.cs
@@ -0,0 +1,16 @@
+﻿using UmbrellaOS.Instruction.Intel.Interfaces;
+
+namespace UmbrellaOS.Instruction.Intel.Prefixes
+{
+    /**
+     * <summary>
+     * LOCK prefix is encoded using F0H.
+     * </summary>
+     */
+    public sealed class PrefixLOCK : InstructionIntelPrefix, IInstructionIntelPrefixLegacy
+    {
+        public static readonly PrefixLOCK Default = new();
+
+        public PrefixLOCK() : base(0xF0) { }
+    }
+}
---
+++ UmbrellaOS/Instruction/Intel/Prefixes/PrefixREP.cs
@@ -0,0 +1,18 @@
+﻿using UmbrellaOS.Instruction.Intel.Interfaces;
+
+namespace UmbrellaOS.Instruction.Intel.Prefixes
+{
+    /**
+     * <summary>
+     * REP or REPE/REPZ is encoded using F3H.<br/>
+     * The repeat prefix applies only to string and input/output instructions.<br/>
+     * (F3H is also used as a mandatory prefix for some instructions.)
+     * </summary>
+     */
+    public sealed class PrefixREP : InstructionIntelPrefix, IInstructionIntelPrefixLegacy
+    {
+        public static readonly PrefixREP Default = new();
+
+        public PrefixREP() : base(0xF3) { }
+    }
+}
