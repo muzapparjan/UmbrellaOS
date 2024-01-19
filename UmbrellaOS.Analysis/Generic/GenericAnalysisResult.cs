708c5eff506a809bacafb3c33859b5ffcb839e3b
Muzappar <muzappar.ce@foxmail.com>
2024-01-19 15:22:30 +0800

    [Feature]: added analysis project

---
+++ UmbrellaOS.Analysis/Generic/GenericAnalysisResult.cs
@@ -0,0 +1,25 @@
+﻿using UmbrellaOS.Analysis.Generic.Interfaces;
+
+namespace UmbrellaOS.Analysis.Generic
+{
+    /**
+     * <summary>
+     * Generic result of the analysis.
+     * </summary>
+     * <seealso cref="IAnalysisResult"/>
+     */
+    public sealed class GenericAnalysisResult : IAnalysisResult
+    {
+        public bool IsPassed { get; private set; } = false;
+        public object? ReturnValue { get; private set; } = null;
+        public Exception? Exception { get; private set; } = null;
+
+        public GenericAnalysisResult() { }
+        public GenericAnalysisResult(bool isPassed = false, object? returnValue = null, Exception? exception = null)
+        {
+            IsPassed = isPassed;
+            ReturnValue = returnValue;
+            Exception = exception;
+        }
+    }
+}
