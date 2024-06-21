c7b4e4a891063fd2f267face1a47ec74c0b04262
Muzappar <muzappar.ce@foxmail.com>
2024-06-21 15:01:06 +0800

    feature: stupid brain part 1

---
+++ UmbrellaOS.Lab.Brains.Stupid/Extensions.cs
@@ -0,0 +1,17 @@
+﻿namespace UmbrellaOS.Lab.Brains.Stupid;
+
+internal static class Extensions
+{
+    public static T[] Clone<T>(this T[] original)
+    {
+        var cloned = new T[original.Length];
+        for (var i = 0; i < original.Length; i++)
+        {
+            if (original[i] is ICloneable cloneable)
+                cloned[i] = (T)cloneable.Clone();
+            else
+                cloned[i] = original[i];
+        }
+        return cloned;
+    }
+}
